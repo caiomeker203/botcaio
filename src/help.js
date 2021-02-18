b8a3dd983e06e01024458967b1f9caf58095e522
caiomeker203 <caioh.mega2018@gmail.com>
2021-02-18 00:35:07 -0200

    first commit

---
+++ src/help.js
@@ -0,0 +1,164 @@
+const help = (prefix) => {
+	return `         
+█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+┃* ✞︎卐𝑪𝑨𝑰𝑶𝑴𝑬𝑲𝑬𝑹卐✞︎*
+┃
+┃ INFO:✅
+┃
+┃ *DONO* :  ✞︎卐𝑪𝑨𝑰𝑶𝑴𝑬𝑲𝑬𝑹卐✞︎
+┃ PARCEIRIAS: *GUIZINHOMEKER*
+┃
+┃ *WHATSAPPS DO PROPRIETÁRIO* :
+┃ 
+┃ *DONO* ( ✞︎卐𝑪𝑨𝑰𝑶𝑴𝑬𝑲𝑬𝑹卐✞︎): wa.me/5519999124606
+┃ 
+┃
+┃
+┃  [𝑨𝑳𝑮𝑼𝑵𝑺 𝑪𝑶𝑴𝑨𝑵𝑫𝑶𝑺 𝑵𝑨̃𝑶 𝑬𝑺𝑻𝑨̃𝑶 𝑭𝑼𝑵𝑪𝑰𝑶𝑵𝑨𝑵𝑫𝑶, 𝑺𝑬 𝑨𝑪𝑯𝑨𝑹 𝑹𝑼𝑰𝑴 𝑬́ 𝑺𝑰𝑴𝑷𝑳𝑬𝑺 𝑷𝑨𝑹𝑨 𝑫𝑬 𝑼𝑺𝑨𝑹, 𝑮𝑹𝑨𝑻𝑶!]
+┃●▬▬▬▬๑۩۩๑▬▬▬▬▬●●▬▬▬▬๑۩۩๑▬▬▬▬▬●●▬▬▬▬๑۩۩๑▬▬▬▬▬●●▬▬▬▬๑۩۩๑▬▬▬▬▬●
+┃
+┃
+┃ █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█
+┃    𝑩𝑬𝑴 𝑽𝑰𝑵𝑫𝑶 𝑨𝑶 𝑴𝑬𝑵𝑼 𝑫𝑶 𝑩𝑶𝑻🤭┃
+┃●▬▬▬▬๑۩۩๑▬๑۩۩๑▬▬▬▬▬●
+┃
+┃
+┃ 
+┃    【 $INÚTEIS* 】 :
+┃●▬▬▬▬๑۩۩๑▬▬▬▬▬●
+┃  ➡ ${prefix}blocklist
+┃     【LISTA DE BLOCK】
+┃  ➡ ${prefix}chatlist
+┃
+║  LISTA CHAT ❌
+║  ➡ ${prefix}ping
+║╠➣     【PING】
+║  ➡ ${prefix}bugreport
+║     【REPORTA BUG】❌
+┃
+┃
+┃      
+║╠➣      *ZOAÇÃO * :
+┃
+┃
+║╠➣  ➡ ${prefix}%GADO
+┃   
+║╠➣  ➡ ${prefix}%CORNO(A)
+┃
+║╠➣  ➡ ${prefix}%NAMORARESSEANO
+┃
+║╠➣  ➡ ${prefix}%GAY
+┃
+┃
+┃
+┃
+║╠➣   【 *MAIS USADOS RECENTEMENTE* 】
+┃                     ●▬▬▬▬๑۩۩๑▬▬▬▬▬●
+┃  ➡ ${prefix}sticker
+┃    【FAZER FIGURINHA】
+┃ ➡ ${prefix}toimg
+┃    【CONVERTE FIGURINHA EM IMAGEM】
+┃  ➡ ${prefix}tomp3
+┃    【PEGAR ÁUDIO DE ALGUM VÍDEO】
+┃
+┃ 
+┃
+║╠➣    【FOTOS ANIME/LOLI e etc】
+┃              ●▬▬▬▬๑۩۩๑▬▬▬▬▬●
+┃
+┃  ➡${prefix}loli
+┃  【foto de loli】
+┃
+┃  ➡${prefix}nekoanime
+┃   【NEKO ANIME】
+┃
+┃  ➡${prefix}randomanime
+┃   【anime aleatório】
+┃
+┃
+┃  ➡${prefix}pokémon
+┃   【foto de pokémon】
+┃
+┃
+┃   
+┃
+┃
+┃     
+┃
+┃
+║╠➣        *MELHORES SOBRE ÁUDIO*
+┃ 
+┃  ➡ ${prefix}play nome da música
+┃    【DOWNLOAD MÚSICA】
+┃  ➡ ${prefix}tts pt texto
+║╠➣    【FALAR SEU TEXTO】
+┃
+┃
+┃
+┃      +18 *NÃO USE NO GRUPO SUJEITO A BAN*
+┃
+┃ 
+┃  ➡${prefix}hentai
+║╠➣   【foto hentai】
+┃
+┃  ➡${prefix}randomhentaio
+┃ ┃┃┃   【random hentai】
+┃
+┃
+┃
+┃
+┃
+┃
+┃┃      
+┃
+║╠➣      【 𝑶𝑼𝑻𝑹𝑶𝑺 】
+┃  
+┃➡ ${prefix}send
+┃  ➡ ${prefix}wame
+┃ ➡ ${prefix}exe
+┃  ➡ ${prefix}qrcode
+┃  ➡ ${prefix}afk
+┃  ➡ ${prefix}timer
+┃  ➡ ${prefix}fml
+┃  ➡ ${prefix}fml2
+┃
+┃
+┃
+║╠➣     【 *DOWNLOADER* 】
+┃  
+┃  ➡ ${prefix}images
+║╠➣    【IMAGENS ALEATÓRIA】
+┃  ➡ ${prefix}ytmp3
+║╠➣    【MP3】
+┃  ➡ ${prefix}ytmp4
+║╠➣    【DOWNLOAD MP4(VIDEO)】
+┃  ➡ ${prefix}tiktok
+┃  ➡ ${prefix}joox
+┃┃         
+┃
+║╠➣           *MEME*
+┃      
+┃
+┃
+┃  ➡  ${prefix}meme
+┃  ➡ 【MEME INDONÉSIO】
+┃  ➡  ${prefix}memeindo
+┃  ➡ 【MEMEINDO INDONESIO】
+┃ 
+┃
+┃
+┃
+┃
+║╠➣     【 *MÚSICA* 】
+┃  
+┃  ➡ ${prefix}lirik
+┃  ➡ ${prefix}chord
+┃
+┃
+┃
+●▬▬▬▬๑۩۩๑▬▬▬▬▬●▬▬▬▬๑۩۩๑▬▬▬▬▬● 
+
+`
+}
+
+exports.help = help
